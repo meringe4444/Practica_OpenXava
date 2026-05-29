dffdee9cc63191792cdf6f51fb0a1e9bfcfcd32e
meringe4444 <ghostform13.st@gmail.com>
2026-05-29 15:46:09 -0600

    Initial commit

---
+++ src/main/java/com/tuempresa/facturacion/modelo/Producto.java
@@ -0,0 +1,23 @@
+package com.tuempresa.facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.Required;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Producto {
+
+    @Id
+    @Column(length = 6)
+    int numero;
+
+    @Column(length = 50)
+    @Required
+    String descripcion;
+
+}
